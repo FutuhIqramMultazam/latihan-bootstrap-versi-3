20fa65fb3ea92415c738fa2f1190dfb687636f79
FutuhIqramMultazam <futuhiqramm@gmail.com>
2024-07-22 15:17:43 +0700

    fitur scrol lembut

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-$(".page-scroll");
+$(".page-scroll").on("click", function (e) {
+  // ambil isi href
+  var tujuan = $(this).attr("href");
+
+  // ambil element bersangkut
+  var elemenTujuan = $(tujuan);
+
+  // pastikan elemen tujuan ada
+  if (elemenTujuan.length) {
+    // pindahkan scroll
+    $("html, body").animate(
+      {
+        scrollTop: elemenTujuan.offset().top - 50,
+      },
+      500
+    ); // durasi animasi dalam milidetik (1000ms = 1s)
+  }
+
+  e.preventDefault();
+});
